505a10663ba2244164eb14c48fb3dd9bd87bdafe
Adetomiwa Lawal <adetomiwa.awal@gmal.com>
2026-04-24 15:06:29 +0200

    workings file

---
+++ workings.py
@@ -0,0 +1,11 @@
+def my_routine():
+    print("start machine")
+    print("make coffee")
+    print("add milk")
+    print("enjoy it")
+
+print("wake up")
+my_routine()
+print("working for a while")
+my_routine()
+
