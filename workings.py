e429e0aa6db50f056e2c910acbf8a0b96a274136
Adetomiwa Lawal <adetomiwa.awal@gmal.com>
2026-04-26 17:33:01 +0200

    new examples added

--- workings.py
+++ workings.py
@@ -1,3 +1,4 @@
+# My everyday 
 def my_routine():
     print("start machine")
     print("make coffee")
@@ -9,3 +10,14 @@ my_routine()
 print("working for a while")
 my_routine()
 
+#Write a function that takes a list of numbers and returns the sum.
+def list_num(numbers):
+    return sum(numbers)
+print(list_num([1,2,3]))
+
+
+
+#Write a function that takes a list and returns the maximum value.
+def get_max(num):
+    return max(num)
+print(get_max([1,5,7,30,78]))
